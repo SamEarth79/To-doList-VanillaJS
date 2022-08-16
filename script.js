bb38f3773bdc487009be90ccc82ab4eb1a1401c1
Samarth M <samarthm@Samarths-MacBook-Air.local>
2022-08-16 16:21:19 +0530

    Added storage API

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Selectors
 const todoField = document.getElementById("todo-field");
-const addButton = document.getElementById("add-btn")
-const todoContainer = document.getElementById("todo-container")
-const todoList = document.getElementById("todo-list")
+const addButton = document.getElementById("add-btn");
+const todoContainer = document.getElementById("todo-container");
+const todoList = document.getElementById("todo-list");
 
 // Event Listeners
-addButton.addEventListener("click", addTodo)
-todoList.addEventListener("click", deleteCheck)
+document.addEventListener("DOMContentLoaded", getTodos);
+addButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 // Functions
 function addTodo(event){
@@ -17,6 +18,8 @@ function addTodo(event){
         alert("Please fill the field");
         return;
     }
+
+    addStorage(todoField.value);
     
     const divElement = document.createElement("div");
     {
@@ -53,8 +56,9 @@ function deleteCheck(event)
     event.preventDefault();
     // alert("Yo")
     const item=event.target;
-    console.log(item);
-    console.log(item.classList)
+    // console.log(item);
+    // console.log(item.classList)
+    deleteStorage(item.parentElement.parentElement.parentElement);
 
     // Delete
     if(item.classList[0]=="delete-button" )
@@ -86,4 +90,69 @@ function deleteCheck(event)
         const todo = item.parentElement.parentElement.parentElement;
         todo.classList.toggle("finished");
     }
+}
+
+function addStorage(todo){
+    let todos;
+    if(localStorage.getItem("todos")===null)
+        todos=[];
+    else
+        todos=JSON.parse(localStorage.getItem("todos"));
+
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    
+}
+
+function deleteStorage(todo){
+    let todos;
+    if(localStorage.getItem("todos")===null)
+        todos=[];
+    else
+        todos=JSON.parse(localStorage.getItem("todos"));
+
+    let todotext = (todo.children[0].innerText);
+    todos.splice(todos.indexOf(todotext), 1);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+}
+
+function getTodos(){
+    let todos;
+    if(localStorage.getItem("todos")===null)
+        todos=[];
+    else
+        todos=JSON.parse(localStorage.getItem("todos"));
+
+    todos.forEach(todo => {
+    const divElement = document.createElement("div");
+    {
+        divElement.classList.add("todo");
+    }
+
+    const liElement = document.createElement("li");
+    liElement.innerText=todo;
+    divElement.append(liElement);
+
+    const actionsDiv = document.createElement("div");
+    actionsDiv.classList.add("actions-div")
+
+    const doneButton = document.createElement("button");
+    doneButton.innerHTML='<i class="fa-solid fa-check"></i>';
+    doneButton.classList.add("done-button");
+    doneButton.style.color="rgb(232, 121, 249)";
+    actionsDiv.append(doneButton);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML='<i class="fa-solid fa-x"></i>';
+    deleteButton.classList.add("delete-button");
+    deleteButton.style.color="rgb(232, 121, 249)";
+    actionsDiv.append(deleteButton);
+
+    divElement.append(actionsDiv);
+
+    todoList.append(divElement);
+    });
+
 }
